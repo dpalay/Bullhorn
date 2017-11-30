d34ed2ea60789d361139c0c48b133f0c306b1360
dpalay <dpalay@rapodcast.net>
2017-11-29 21:42:49 -0600

    Essentially done
    Well, that was fast.  The idea of this bot is that when a certain user as defined in the config file DMs the bot, then the bot will relay that message to the list of BroadcastTo channels.

--- Bullhorn.js
+++ Bullhorn.js
@@ -1,3 +1,5 @@
+import { map } from '../../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/async';
+
 let config = {};
 if (process.argv[2]) {
     let configfile = './' + process.argv[2]
@@ -15,15 +17,36 @@ const client = new Discord.Client({
 // Get other libraries
 const _ = require('underscore');
 
-/* 
-****************************************************** 
-PUT LOGIC HERE
-****************************************************** 
-*/
+
+//Message Recieved
+client.on('message', message => {
+    // logger shouldn't check it's own stuff)
+    if (message.author.id != client.user.id) {
+        if (message.author.id == config.watchUser && message.channel.type == "dm") {
+            _.each(config.broadcastTo, (chan) => {
+                client.channels.find("id", chan).send(message.content)
+            })
+
+        }
+    }
+})
+
+
+// Connected!
+client.on('ready', () => {
+    console.log('Bullhorn is ready!');
+    ME = client.user
+    ME.setGame("Shawn's Minion")
+    client.fetchUser(config.watchUser).then((Shawn) => {
+        Shawn.createDM().then((dm) => {
+            dm.send("Hey there, Oh Great and Powerful One.  I'm online and ready to go.").then()
+            ShawnDM = dm;
+        })
+    }).catch((e) => console.error(e))
+});
 
 
 
-
+// connect
 console.log("Logging in!")
-    // connect
 client.login(config.token)
