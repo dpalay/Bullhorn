f5ea8bebc98b09ad37a4caa05d4a078c0583c7d7
dpalay <dpalay@rapodcast.net>
2019-01-06 21:27:24 -0600

    Cleaned up code for a few deprecations (find -> get, setGame -> setActivity). Added documentation for the config.json file. Added some error handling to fail a bit more gracefully

--- Bullhorn.js
+++ Bullhorn.js
@@ -1,4 +1,23 @@
-import { map } from '../../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/async';
+/*
+CONFIG FILE DEFINITIONS:
+{
+    "token": "string", // the bot token from discord's developer page
+    "id": "UserId", // The bot's user id
+    "watchChannel": "", // 
+    "DaveID": "UserId",  // ID of Dave, the author
+    "watchUser": "UserId",  // Id of the user to re-broadcast to others
+    "broadcastTo":   // List of channel IDs to post the messages to
+       ["ChannelId",
+        "ChannelId",
+        "ChannelId",
+        "ChannelId",
+        "ChannelId",
+        "ChannelId",
+        "ChannelId"
+    ]
+}
+*/
+
 
 let config = {};
 if (process.argv[2]) {
@@ -23,8 +42,16 @@ client.on('message', message => {
     // logger shouldn't check it's own stuff)
     if (message.author.id != client.user.id) {
         if (message.author.id == config.watchUser && message.channel.type == "dm") {
+            console.log(`Sending message out: ${message.content}`)
             _.each(config.broadcastTo, (chan) => {
-                client.channels.find("id", chan).send(message.content)
+                let sendToChannel = client.channels.get(chan)
+                if (sendToChannel) {
+                    sendToChannel.send(message.content)
+                        .then(message => console.log(`Sent message to ${message.channel.guild.name}#${message.channel.name}`))
+                        .catch(console.error)
+                } else {
+                    console.log(`ERROR: Could not send to channel ${chan}.  Channel was not found. This bot might not be on the right server for it.`)
+                }
             })
 
         }
@@ -36,7 +63,7 @@ client.on('message', message => {
 client.on('ready', () => {
     console.log('Bullhorn is ready!');
     ME = client.user
-    ME.setGame("Shawn's Minion")
+    ME.setActivity("Shawn's Minion")
     client.fetchUser(config.watchUser).then((Shawn) => {
         Shawn.createDM().then((dm) => {
             dm.send("Hey there, Oh Great and Powerful One.  I'm online and ready to go.").then()
