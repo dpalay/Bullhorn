3e2b60d6fb250580087ba713cbd074af8e867f53
dpalay <dpalay@rapodcast.net>
2019-01-07 23:44:53 -0600

    Cleaned up the code a LOT.
    Moved the logic for building the list of channels to send to the onReady section.
    
    Removed the dependency on Underscore
    
    First, made it so the bot automatically looks through all the server's it's in.
    Then it looks for "#annoucements" and checks if it has permissions for it.
    If everything is good, it adds it to the broadcast list.
    
    THEN  it goes through the channels in the config file.  If it's in the broadcast list already, it skips it.

--- Bullhorn.js
+++ Bullhorn.js
@@ -1,4 +1,4 @@
-/*
+/**
 CONFIG FILE DEFINITIONS:
 {
     "token": "string", // the bot token from discord's developer page
@@ -6,19 +6,13 @@ CONFIG FILE DEFINITIONS:
     "watchChannel": "", // 
     "DaveID": "UserId",  // ID of Dave, the author
     "watchUser": "UserId",  // Id of the user to re-broadcast to others
-    "broadcastTo":   // List of channel IDs to post the messages to
-       ["ChannelId",
-        "ChannelId",
-        "ChannelId",
-        "ChannelId",
-        "ChannelId",
-        "ChannelId",
-        "ChannelId"
-    ]
+    "broadcastTo": ["ChanID", // ID of additional channels to add to the broadcast list
+                  "ChanID", 
+                  "ChanID"]
 }
 */
 
-
+let ME;
 let config = {};
 if (process.argv[2]) {
     let configfile = './' + process.argv[2]
@@ -27,15 +21,13 @@ if (process.argv[2]) {
     console.error("No config file given.  start with node BullHorn.js configFileName")
 }
 
+let broadcastToChannels = []
+
 // Set up discord.js client
 const Discord = require('discord.js');
 const client = new Discord.Client({
     autoReconnect: true
 });
-
-// Get other libraries
-const _ = require('underscore');
-
 
 //Message Recieved
 client.on('message', message => {
@@ -43,17 +35,11 @@ client.on('message', message => {
     if (message.author.id != client.user.id) {
         if (message.author.id == config.watchUser && message.channel.type == "dm") {
             console.log(`Sending message out: ${message.content}`)
-            _.each(config.broadcastTo, (chan) => {
-                let sendToChannel = client.channels.get(chan)
-                if (sendToChannel) {
-                    sendToChannel.send(message.content)
-                        .then(message => console.log(`Sent message to ${message.channel.guild.name}#${message.channel.name}`))
-                        .catch(console.error)
-                } else {
-                    console.log(`ERROR: Could not send to channel ${chan}.  Channel was not found. This bot might not be on the right server for it.`)
-                }
+            broadcastToChannels.forEach((chan) => {
+                chan.send(message.content)
+                    .then(message => console.log(`Sent message to ${message.channel.guild.name}#${message.channel.name}`))
+                    .catch(console.error)
             })
-
         }
     }
 })
@@ -64,9 +50,51 @@ client.on('ready', () => {
     console.log('Bullhorn is ready!');
     ME = client.user
     ME.setActivity("Shawn's Minion")
+
+    // get list of guilds (servers) and see if there is a channel named "#announcements"
+    client.guilds.forEach((guild) => {
+        let FindAnnouncementsPromise = new Promise((result) => { result(guild.channels.find((chan) => chan.name.toLowerCase == "announcements")) })
+        FindAnnouncementsPromise.then((chan) => {
+            if (chan) {
+                console.log(`Server:${guild.name}\tAnnouncement channel found!`)
+                if (chan.permissionsFor(ME).has("SEND_MESSAGES")) {
+                    console.log(`Server:${guild.name}\tI can send to that channel! Added to broadcast list`)
+                    broadcastToChannels.push(chan)
+                } else {
+                    console.error(`Server:${guild.name}\tI can't send to that channel!  I need permission to SEND_MESSAGES.`)
+                }
+            } else {
+                console.error(`Server:${guild.name}\thas no "announcements" channel.`)
+            }
+        }).catch((e) => console.error(e))
+    })
+
+    // Add channels from config file
+    config.broadcastTo.forEach((id) => {
+        let chan = client.channels.get(id)
+        if (chan) {
+            //Check if it already exists in the list
+            if (broadcastToChannels.includes(chan)) {
+                console.log(`Server:${chan.guild.name}\tChannel already exists as annoucement. Skipping`)
+            } else {
+
+                console.log(`Server:${chan.guild.name}\tChannel:${chan.name} found from config file!`)
+                if (chan.permissionsFor(ME).has("SEND_MESSAGES")) {
+                    console.log(`Server:${chan.guild.name}\tI can send to that channel! Added to broadcast list`)
+                    broadcastToChannels.push(chan)
+                } else {
+                    console.error(`Server:${chan.guild.name}\tI can't send to that channel!  I need permission to SEND_MESSAGES.`)
+                }
+            }
+        } else {
+            console.error(`Server:UNKNOWN\tCan't find any channel with that ID.  I'm not on that server.`)
+        }
+    })
+
+    // Find Shawn and obey his every whim.
     client.fetchUser(config.watchUser).then((Shawn) => {
         Shawn.createDM().then((dm) => {
-            dm.send("Hey there, Oh Great and Powerful One.  I'm online and ready to go.").then()
+            dm.send("Hey there, Oh Great and Powerful One.  I'm online.  Please give me a minute and I should be ready to go.").then()
             ShawnDM = dm;
         })
     }).catch((e) => console.error(e))
@@ -75,5 +103,4 @@ client.on('ready', () => {
 
 
 // connect
-console.log("Logging in!")
-client.login(config.token)
+console.log("Logging in!") client.login(config.token).then().catch((e) => console.error(e))
